e2280f5b9b0bcc8d9f1cccc3b348d4b3a75163bb
Mateusz Ochęcki <mateusz.ochecki@gmail.com>
2023-01-30 23:56:26 +0100

    Add pre-commit config

--- main.tf
+++ main.tf
@@ -2,9 +2,3 @@ resource "azurerm_resource_group" "shared_rg" {
   name     = "rg-${var.prefix}-services-${var.environment}-001"
   location = var.location
 }
-
-##TEST
-
-resource "azurerm_resource_group" "shared_rg" {
-  
-}
